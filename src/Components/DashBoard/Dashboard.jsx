e99aacd1555a4bf0ce27d8a9bff05b617d091b4f
Administrator <danielobeng016@gmail.com>
2024-08-21 08:48:06 -0400

    API Integration

--- src/Components/DashBoard/Dashboard.jsx
+++ src/Components/DashBoard/Dashboard.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React ,{ useEffect }from 'react';
 import Footer from "../Home/Footer";
 import Grid from "../../Grid";
 import SideBar from "./SideBar";
 import Section6 from "../Home/Section6";
+import { analytics } from '../../firebase';
+import { logEvent } from "firebase/analytics";
 import "./Dashboard.css"
 
 
 function Dashboard(){
+    useEffect(() => {
+        // Log an event for visiting the User Dashboard
+        logEvent(analytics, 'dashboard_view', { page_name: 'UserDashboard' });
+    }, []);
+
     const vid ={
         vid1:"Gradient 2.mp4"
     }
